e3c8e00fa25299fe5177080135b87cd1116e9eb9
Think201 <think201@Think201s-MacBook-Air.local>
2020-05-16 03:07:36 +0530

    add first get route

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const express = require('express');
 // Create an instance of the express constructor
 const app = express();
 
+// Sample json data for mocking up in the server
+const mockUserData = [
+    { name: 'Mark' }, 
+    { name: 'Henry' }
+];
+
+// Create a get request to get access to user's data on success
+
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'Successfully got the users. Nice!',
+        users: mockUserData
+    });
+});
+
 // Start up your server locally on port 8000 as the first arguement
 
 app.listen(8000, function() {
