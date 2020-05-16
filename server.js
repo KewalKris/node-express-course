9c5b0227bb4b1d5c679ac8bdc4df4ebe2ab53a75
Think201 <think201@Think201s-MacBook-Air.local>
2020-05-16 21:30:52 +0530

    Add login post route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 // Create an instance of the express constructor
 const app = express();
 
+// Create an instance of body-parser in order to make a post request
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 // Sample json data for mocking up in the server
 const mockUserData = [
     { name: 'Mark' }, 
@@ -20,7 +24,7 @@ app.get('/users', function(req, res) {
     });
 });
 
-// Create a param route to pass variable through url which can be accessed through req.params
+// Create a dynamic get route to pass variable through url which can be accessed through req.params
 
 app.get('/users/:id', function(req, res) {
     console.log(req.params.id);
@@ -29,6 +33,34 @@ app.get('/users/:id', function(req, res) {
         message: 'Got one user',
         user: req.params.id
     });
+});
+
+// Create a function to handle post request made to the login endpoint
+
+app.post('/login', function(req, res) {
+
+    // Typically passwords are encrypted using something like bcrypt before sending to database
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // This data should come from database
+    const mockUsername = 'billyTheKid';
+    const mockPassword = 'superSecret';
+
+    // Typically you should first check if the username exists, if yes then fetch the password to compare both
+    if(username === mockUsername && password === mockPassword) {
+        // In practice, use JSON web token sign method to make an encrpted token
+        res.json({
+            success: true,
+            message: 'password and username match',
+            token: 'encrypted token goes here'
+        });
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        });
+    }
 });
 
 // Start up your server locally on port 8000 as the first arguement
